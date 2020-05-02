944ab98109788764fc863c85fd6e8d0764f8972a
DimaBir <dvlasenko86@gmail.com>
2020-05-03 01:14:27 +0300

    Init Commit

---
+++ RESTApiExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Script.Serialization;
+
+namespace RESTApiExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instance fo Sender class that have 1 method that send json to server
+            Sender exampleSender = new Sender();
+
+            // Create some sutom json, fields are mandatory
+            string json = new JavaScriptSerializer().Serialize(new
+            {
+                name = "Yosy",
+                age = 75
+            });
+
+            // Sends created json to server and returns response on success, exception on failure
+            string response = exampleSender.Send(json);
+            Console.WriteLine(response);
+        }
+    }
+}
---
+++ RESTApiExample/Sender.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace RESTApiExample
+{
+    class Sender
+    {
+        public string Send(string json)
+        {
+            // Creates request to specific API (/api/test), full path needed.
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("https://englishtips.azurewebsites.net/api/test");
+            // Tells to Client what type of request to send, we are sending json, so 'application/json'
+            httpWebRequest.ContentType = "application/json";
+
+            // Method POST we want send something to server. GET we want get something from server
+            httpWebRequest.Method = "POST";
+
+            // Writes json to request stream, we can send more than one request, also sending can be asynchronous (in the future)
+            // Here using is necessary. readers/writers are open stream, that consumes memory. Stream need to be closed, else memory leak, 
+            // if we use using, it will close automatically, when we leave {} block. 
+            using (StreamWriter streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+            }
+
+            // Sends our requests to server, and gets response, if there any error - exception will be thrown here.
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            
+            // Just reads respons and returns them, we expect here only one response so we will return it, but if there will be more
+            // in the future we can use List<string>
+            string result = String.Empty;
+
+            // Statemnt with ?? called Nullable it checks if httpResponse.GetResponseStream() is not Null, if it is null we will throw exception, else we will continue
+            using (StreamReader streamReader = new StreamReader(httpResponse.GetResponseStream() ?? throw new InvalidOperationException()))
+            {
+                result = streamReader.ReadToEnd();
+            }
+
+            // Just to be more informative if server returned 200 (OK) status but no text (maybe some logic error om server side) we will
+            // return proper message instead of server crash
+            return !string.IsNullOrEmpty(result) ? result : "Empty string";
+        }
+    }
+}
