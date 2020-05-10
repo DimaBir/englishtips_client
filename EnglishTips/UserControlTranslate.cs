30a69ece21d9180a634df7b387481b1b5c8cdf8d
LevNetanel <38345761+LevNetanel@users.noreply.github.com>
2020-05-10 13:44:37 +0300

    Update UserControlTranslate.cs

--- EnglishTips/UserControlTranslate.cs
+++ EnglishTips/UserControlTranslate.cs
@@ -36,18 +36,13 @@ namespace EnglishTips
                 return;
             }
 
-            //SayAloud.Say(text: selection.Text);
-
-            //string text_to_translate = "Hello, how are you? Enter here text and the language you want to translate into.";
-            string text_to_translate = selection.Text;
-
             // Creates custom json
             // JSON body:
             // "text": (string)"Text you wanna to send to server"
             // "language": (string)"he"
             string json = new JavaScriptSerializer().Serialize(new
             {
-                text = text_to_translate,
+                text = selection.Text,
                 language = "he"
             });
 
@@ -56,12 +51,6 @@ namespace EnglishTips
             // Sends created json to server and returns:
             // returns: string
             string translation = GenericSender<string>.Send(json, api: api, "POST");
-
-
-            // Technique to handle  with printing hebrew to console.
-            // If you will look at the string itself on debug mode, you will see that it stored as legal hebrew,
-            // only in printing its messed up, so here is the solution.
-            // Maybe in the word addin you can print hebrew as is, cause this solution reverses chars and numbers too. 
 
             //Globals.ThisAddIn.TranslateCustomTaskPane
             //TextBox txt = (TextBox)this.Parent.FindControl("txtid");
