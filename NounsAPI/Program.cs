57ac810b8ea2afa0792c2fe8133399ea1d45ae06
DimaVlas <dvlasenko86@gmail.com>
2020-05-10 21:17:05 +0300

    Verb word indexes by chars

--- NounsAPI/Program.cs
+++ NounsAPI/Program.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using EnglishTips;
 
-namespace VerbsAPI
+namespace NounsAPI
 {
     class Program
     {
@@ -32,28 +33,27 @@ namespace VerbsAPI
             {
                 text = text_to_check
             });
-            string api = "https://englishtips.azurewebsites.net/api/verbs2";
+            string api = "https://englishtips.azurewebsites.net/api/verbs";
 
             // Sends created json to the server:
             // returns: Dictionary - that contains verbs and their indexes in sended text:
             //      string = verb itself,
             //      List<int> - list of indexes of verb in the text
-            List<VerbsResponse> response =
-                GenericSender<List<VerbsResponse>>.Send(json, api: api, "POST");
+            Dictionary<string, List<int>> response =
+                GenericSender<Dictionary<string, List<int>>>.Send(json, api: api, "POST");
 
             Console.WriteLine($"Sending text to '{api}' to find verbs...\n\n\n");
             Console.WriteLine($"Original text: {text_to_check}");
 
             // Print out verbs.
-            foreach (VerbsResponse verbObject in response)
+            foreach (KeyValuePair<string, List<int>> verb in response)
             {
-                Console.Write($"Verb: '{verbObject.Verb}' - Length: {verbObject.VerbLength} - Index: ");
-                foreach (int index in verbObject.Indexes)
+                Console.Write($"Noun: '{verb.Key}' --- Index: ");
+                foreach (int index in verb.Value)
                     Console.Write($"{index}, ");
 
                 Console.WriteLine();
             }
-
 
             sw.Stop();
             Console.WriteLine("\n\nTime Elapsed={0}", sw.Elapsed);
---
+++ VerbsAPI/VerbsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace VerbsAPI
+{
+    public class VerbsResponse
+    {
+        public string Verb { get; set; }
+        public int VerbLength { get; set; }
+        public List<int> Indexes { get; set; }
+    }
+}
