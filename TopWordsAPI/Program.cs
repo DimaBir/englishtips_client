36595d63b2de3ecf0114b6d72900e6a674cdfcb6
DimaVlas <dvlasenko86@gmail.com>
2020-05-18 14:31:02 +0300

    Added top ten words API

---
+++ TopWordsAPI/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using RESTApiExample;
+
+namespace TopWordsAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            // Prepare request parameters
+            string text_to_check =
+                "In publishing and graphic design, Lorem ipsum is a placeholder text" +
+                " commonly used to demonstrate the visual form of a document or a typeface" +
+                " without relying on meaningful content. Lorem ipsum may be used before" +
+                " final copy is available, but it may also be used to temporarily replace" +
+                " copy in a process called greeking, which allows designers to consider" +
+                " form without the meaning of the text influencing the design.\n\n\n";
+
+
+            string json = new JavaScriptSerializer().Serialize(new
+            {
+                text = text_to_check
+            });
+
+            string api = "https://englishtips.azurewebsites.net/api/topwords";
+
+
+            // Send request
+            TopWordsResponse response =
+                GenericSender<TopWordsResponse>.Send(json, api: api, "POST");
+
+            // Print out the response.
+            Console.WriteLine($"Sending text to '{api}' to find wordiness...\n\n\n");
+            Console.WriteLine($"Original text: {text_to_check}");
+
+            foreach (TopWordsResponse.TopWordsData match in response.Results)
+            {
+                Console.Write($"Word: '{match.Word}' Length: {match.Length} Indexes: ");
+                foreach (int index in match.Indexes)
+                {
+                    Console.Write($"{index}, ");
+                }
+                Console.WriteLine();
+                Console.WriteLine($"Place: {match.Place}");
+            }
+
+            // Print time elapsed.
+            sw.Stop();
+            Console.WriteLine("\n\nServer Execution Time = {0} (sec)", response.ServerExecutionTime);
+            Console.WriteLine("Client Execution Time = {0} (sec)", sw.Elapsed);
+            Console.ReadKey();
+            return;
+        }
+    }
+}
---
+++ TopWordsAPI/TopWordsResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TopWordsAPI
+{
+    class TopWordsResponse
+    {
+        [JsonProperty("Result")]
+        public IList<TopWordsData> Results { get; set; }
+        [JsonProperty("ServerExecutionTime")]
+        public float ServerExecutionTime { get; set; }
+
+        public class TopWordsData
+        {
+            public string Word { get; set; }
+            public int Length { get; set; }
+            public IList<int> Indexes { get; set; }
+            public int Place { get; set; }
+        }
+    }
+}
