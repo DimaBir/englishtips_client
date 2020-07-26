482037d85595fbf21bff41122ca1499b7fbc47fc
LevNetanel <38345761+LevNetanel@users.noreply.github.com>
2020-07-26 19:31:04 +0300

    Removed coloring from ribbon

--- EnglishTips/ribbon.Designer.cs
+++ EnglishTips/ribbon.Designer.cs
@@ -37,13 +37,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ribbon));
             this.tab2 = this.Factory.CreateRibbonTab();
             this.group1 = this.Factory.CreateRibbonGroup();
+            this.Coloring = this.Factory.CreateRibbonToggleButton();
             this.group2 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
             this.group3 = this.Factory.CreateRibbonGroup();
             this.editBox1 = this.Factory.CreateRibbonEditBox();
             this.group4 = this.Factory.CreateRibbonGroup();
-            this.btn_refresh_coloring = this.Factory.CreateRibbonButton();
-            this.Coloring = this.Factory.CreateRibbonToggleButton();
-            this.button1 = this.Factory.CreateRibbonButton();
             this.toggleButton1 = this.Factory.CreateRibbonToggleButton();
             this.toggleButton2 = this.Factory.CreateRibbonToggleButton();
             this.tab2.SuspendLayout();
@@ -64,16 +63,32 @@
             // 
             // group1
             // 
-            this.group1.Items.Add(this.btn_refresh_coloring);
             this.group1.Items.Add(this.Coloring);
             this.group1.Label = "Coloring";
             this.group1.Name = "group1";
+            // 
+            // Coloring
+            // 
+            this.Coloring.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.Coloring.Label = "Coloring";
+            this.Coloring.Name = "Coloring";
+            this.Coloring.ShowImage = true;
+            this.Coloring.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.Coloring_Click);
             // 
             // group2
             // 
             this.group2.Items.Add(this.button1);
             this.group2.Label = "group2";
             this.group2.Name = "group2";
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Label = "Read aloud";
+            this.button1.Name = "button1";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
             // 
             // group3
             // 
@@ -94,30 +109,6 @@
             this.group4.Items.Add(this.toggleButton2);
             this.group4.Label = "Dictionary";
             this.group4.Name = "group4";
-            // 
-            // btn_refresh_coloring
-            // 
-            this.btn_refresh_coloring.Label = "Refresh";
-            this.btn_refresh_coloring.Name = "btn_refresh_coloring";
-            this.btn_refresh_coloring.ShowImage = true;
-            this.btn_refresh_coloring.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button4_Click);
-            // 
-            // Coloring
-            // 
-            this.Coloring.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
-            this.Coloring.Label = "Coloring";
-            this.Coloring.Name = "Coloring";
-            this.Coloring.ShowImage = true;
-            this.Coloring.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.Coloring_Click);
-            // 
-            // button1
-            // 
-            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Label = "Read aloud";
-            this.button1.Name = "button1";
-            this.button1.ShowImage = true;
-            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
             // 
             // toggleButton1
             // 
@@ -166,7 +157,6 @@
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup group3;
         internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox1;
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup group4;
-        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn_refresh_coloring;
         internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton toggleButton1;
         internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton toggleButton2;
         internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton Coloring;
